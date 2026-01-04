63fae48a6d8cf1e9c67f1a6fde7e1cf62436e039
Hugo Torres <noreply@redactado.es>
2026-01-04 13:29:26 +0100

    Add MessagesPartial class for configuration of plugin messages

---
+++ src/main/java/es/redactado/shopkeepersEI/config/partial/MessagesPartial.java
@@ -0,0 +1,11 @@
+package es.redactado.shopkeepersEI.config.partial;
+
+import de.exlll.configlib.Configuration;
+
+@Configuration
+public class MessagesPartial {
+
+    public String prefix = "<#eb64f8><bold>ShopkeepersEI<reset> <dark_gray>» ";
+
+    public String pluginReloaded = "&aPlugin reloaded!";
+}
