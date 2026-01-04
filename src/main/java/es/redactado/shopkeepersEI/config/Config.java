633b4bd62747a3a41ba39cd8f79c1ae4614066c0
Hugo Torres <noreply@redactado.es>
2026-01-04 12:09:58 +0100

    Refactor Config class to use Configuration library and simplify message handling

--- src/main/java/es/redactado/shopkeepersEI/config/Config.java
+++ src/main/java/es/redactado/shopkeepersEI/config/Config.java
@@ -1,23 +1,13 @@
 package es.redactado.shopkeepersEI.config;
 
-import lombok.Getter;
-import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-import org.spongepowered.configurate.objectmapping.meta.Setting;
+import de.exlll.configlib.Configuration;
+import es.redactado.shopkeepersEI.config.partial.MessagesPartial;
 
-@Getter
-@ConfigSerializable
+@Configuration
 public class Config {
 
-    @Setting public Boolean isDebug = false;
+    public Boolean isDebug = false;
 
-    @Setting public MessagesConfig messages = new MessagesConfig();
+    public MessagesPartial messages = new MessagesPartial();
 
-    @Getter
-    @ConfigSerializable
-    public static class MessagesConfig {
-
-        @Setting public String prefix = "<#eb64f8><bold>ShopkeepersEI<reset> <dark_gray>» ";
-
-        @Setting public String pluginReloaded = "&aPlugin reloaded!";
-    }
 }
