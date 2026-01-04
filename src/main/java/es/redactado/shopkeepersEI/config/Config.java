efc6927d4b4ce3051c5e523fdf77ee0ab682dd34
Hugo Torres <noreply@redactado.es>
2026-01-04 13:28:48 +0100

    Refactor Config class to introduce log level configuration with comments for available values

--- src/main/java/es/redactado/shopkeepersEI/config/Config.java
+++ src/main/java/es/redactado/shopkeepersEI/config/Config.java
@@ -1,12 +1,15 @@
 package es.redactado.shopkeepersEI.config;
 
+import de.exlll.configlib.Comment;
 import de.exlll.configlib.Configuration;
+import es.redactado.shopkeepersEI.Logger;
 import es.redactado.shopkeepersEI.config.partial.MessagesPartial;
 
 @Configuration
 public class Config {
 
-    public Boolean isDebug = false;
+    @Comment("Available values: TRACE, DEBUG, INFO, SUCCESS, WARN, ERROR, FATAL")
+    public Logger.LogLevel logLevel = Logger.LogLevel.INFO;
 
     public MessagesPartial messages = new MessagesPartial();
 
