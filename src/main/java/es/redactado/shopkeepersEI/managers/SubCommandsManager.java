9f312a2c08360f434d22cb28ab8de91482ab6633
Hugo Torres <noreply@redactado.es>
2026-01-04 13:29:56 +0100

    Remove unnecessary files.

--- src/main/java/es/redactado/shopkeepersEI/managers/SubCommandsManager.java
+++
@@ -1,9 +0,0 @@
-package es.redactado.shopkeepersEI.managers;
-
-import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
-
-public abstract class SubCommandsManager {
-    public abstract void execute(
-            @NotNull CommandSender sender, @NotNull String cmd, @NotNull String[] args);
-}
